a914b242da35c5a6ee332f92145acd3c33237c80
Florsecco <florenciasecco3@gmail.com>
2023-10-17 14:27:40 -0300

    subiendo

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app= express()
+const path= require('path')
+
+const publicPath = path.resolve(__dirname, './public')
+
+app.use( express.static(publicPath))
+
+app.listen(3008, () => console.log("Servidor corriendo en puerto 3008"));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+
+app.get('/ofertas',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/ofertas.html'))
+})
+
+app.get('/tiendas-oficiales',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/tiendas-oficiales.html'))
+})
+
+app.get('/vender',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/vender.html'))
+})
+
+app.get('/ayuda',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/ayuda.html'))
+})
+
+app.get('/crea-tu-cuenta',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/crea-tu-cuenta.html'))
+})
+
+app.get('/ingresa',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/ingresa.html'))
+})
+
+app.get('/mis-compras',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/mis-compras.html'))
+})
