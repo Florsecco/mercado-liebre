64ab026a9bfad66d075226459a8edc768dd7fd5e
“Florsecco” <florenciasecco3@gmail.com>
2023-10-17 21:34:44 -0300

    ultimos cambios

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app= express()
 const path= require('path')
 
-const publicPath = path.resolve(__dirname, './public')
 
-app.use( express.static(publicPath))
+app.use( express.static(__dirname + '/public'))
 
 app.listen(3008, () => console.log("Servidor corriendo en puerto 3008"));
 
